4f912ee38e47ef32633a9e6f3ca44f448a537920
unknown <narek.mamikonyan@vivaro-pc-0179.ad.vivaro.am>
2014-10-20 10:43:18 +0400

    Cloning repo from local to server

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularRangeSliderApp
+ * @description
+ * # angularRangeSliderApp
+ *
+ * Main module of the application.
+ */
+angular.module('angularRangeSliderApp',[])
+    .run(['$templateCache',function($templateCache){
+        $templateCache.put('../../views/rg-slissder.html',
+                '<div class="rg-slider">' +
+                     '<div class="rg-slider-wrapper"> ' +
+                        '<div class="rg-slider-trackbar" ng-class="trackBarClass">' +
+                             '<div class="rg-tracker"  ng-class="trackerClass"></div> ' +
+                        '</div>' +
+                     '</div>     ' +
+                    '<div class="rg-navigator" ng-if="showNavigator" ng-class="navigatorClass">' +
+                        '<ul> ' +
+                            '<li ng-repeat="item in navList  track by $index" ng-style="{width: listItemWidth}">' +
+                                ' <span ng-class="{rgActive: (item === curValue)}">{{item}}</span> ' +
+                                '<span ng-class="{rgActive: (item+1 === curValue)}" class="rg-list-lastitem" ng-if="$last">{{item + 1 }}</span>' +
+                            '</li>' +
+                        '</ul>' +
+                      '</div>' +
+                 '</div>');
+    }]);
